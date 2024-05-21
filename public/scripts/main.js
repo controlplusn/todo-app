f9a7db4aa19de1d4e41be5561a09faf037ff2453
DevEminent <22-06230@g.batstate-u.edu.ph>
2024-05-21 11:30:38 +0800

    Add task input and fix card div

---
+++ public/scripts/main.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const todo = document.querySelector('.todo-app');
+const items = document.querySelector('#list-container');
+const btn = document.querySelector('button')
+const taskInput = document.querySelector('#input-box');
+
+console.log(taskInput.value);
