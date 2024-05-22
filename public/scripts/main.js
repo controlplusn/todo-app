a994dd63d1d72aff6e8c4fb57b859b5be2fa6302
DevEminent <22-06230@g.batstate-u.edu.ph>
2024-05-22 10:54:21 +0800

    Add checkbox and edit option

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,25 @@
-const container = document.querySelector('.container');
-const todo = document.querySelector('.todo-app');
-const items = document.querySelector('#list-container');
-const btn = document.querySelector('button')
-const taskInput = document.querySelector('#input-box');
+const button = document.querySelector('#button');
+const input = document.querySelector('#input-box');
+const form = document.querySelector('#form');
+const list = document.querySelector('#list-container');
 
-console.log(taskInput.value);
+
+button.addEventListener('click', addItem);
+
+function addItem(e) {
+    // get input value
+    var newItem = document.querySelector('#input-box').value;
+
+    // create new li element
+    var li = document.createElement('li');
+    
+    // add class
+    li.className = "item";
+
+    // add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    list.appendChild(li);
+
+    input.value = '';
+}
