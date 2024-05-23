5341d8059b5dcfb61e6318374b3d22dafa56623b
DevEminent <22-06230@g.batstate-u.edu.ph>
2024-05-23 15:06:58 +0800

    Add task

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,19 +7,43 @@ const list = document.querySelector('#list-container');
 button.addEventListener('click', addItem);
 
 function addItem(e) {
+    e.preventDefault();
+    // Text
+
     // get input value
-    var newItem = document.querySelector('#input-box').value;
+    var newItem = input.value;
+
+    // if text input is empty
+    if (newItem.trim === "") {
+        return;
+    }
 
     // create new li element
     var li = document.createElement('li');
-    
-    // add class
-    li.className = "item";
 
-    // add text node with input value
-    li.appendChild(document.createTextNode(newItem));
+    // create new checkbox element
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'chbox';
+     
+    // create new label element
+    var label = document.createElement('label');
+    label.className = 'item';
+    label.appendChild(document.createTextNode(newItem));
 
+    // create new icon element
+    var icon = document.createElement('i');
+    icon.className = 'bx bx-dots-horizontal-rounded';
+
+    // Append checkbox and label to li
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.appendChild(icon);
+
+    // Append new li to the list
     list.appendChild(li);
 
+    // clear input
     input.value = '';
+
 }
