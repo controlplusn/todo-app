f769fa1409dc4622cfe6cd42ce448636615ec83b
DevEminent <22-06230@g.batstate-u.edu.ph>
2024-05-30 13:13:47 +0800

    Change options icon to edit and delete buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,7 +3,6 @@ const input = document.querySelector('#input-box');
 const form = document.querySelector('#form');
 const listContainer = document.querySelector('#list-container');
 const list = document.querySelector('.item-list');
-const icon = document.querySelector('.bx.bx-dots-horizontal-rounded');
 
 // Add item
 button.addEventListener('click', addItem);
@@ -32,60 +31,14 @@ function addItem(e) {
     var label = document.createElement('label');
     label.className = 'item';
     label.appendChild(document.createTextNode(newItem));
-    console.log(label);
-
-    // create new icon element
-    var icon = document.createElement('i');
-    icon.className = 'bx bx-dots-horizontal-rounded';
 
     // Append checkbox and label to li
     li.appendChild(checkbox);
     li.appendChild(label);
-    li.appendChild(icon);
 
     // Append new li to the list
     listContainer.appendChild(li);
 
     // clear input
     input.value = '';
-}
-
-// settings
-icon.addEventListener("click", editTask);
-
-function editTask(e) {
-    e.preventDefault();
-
-    // create new div element with menu class
-    var menu = document.createElement('div');
-    menu.className = 'menu';
-    
-    // create ul element inside the div
-    var settings = document.createElement('ul');
-    settings.id = 'settings';
-    settings.className = 'settings';
-
-    // Add new edit label element inside the li
-    var editLi = document.createElement('li');
-    var editLabel = document.createElement('label');
-    editLabel.className = 'edit';
-    editLabel.appendChild(document.createTextNode("Edit"));
-    editLi.appendChild(editLabel);
-    
-    // Add new delete label element inside the li
-    var deleteLi = document.createElement('li');
-    var deleteLabel = document.createElement('label');
-    deleteLabel.className = 'delete';
-    deleteLabel.appendChild(document.createTextNode("Delete"));
-    deleteLi.appendChild(deleteLabel);
-
-    // append li elements to ul
-    settings.appendChild(editLi);
-    settings.appendChild(deleteLi);
-
-    // append ul to div
-    menu.appendChild(settings);
-
-    // append menu div to the parent of the clicked icon
-    e.target.parentElement.appendChild(menu);
 }
